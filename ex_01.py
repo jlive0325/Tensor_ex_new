0dd6c731dd9724894bf45752fb5dfa8966497ba0
tensor_yeonhee <jlive0325@nate.com>
2022-03-29 10:59:56 +0900

    22.03.29 commit

--- ex_01.py
+++ ex_01.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
-# tf.__version__
+
+
+
+
+""" # tf.__version__
 
 hello = tf.constant('Hello, TensorFlow!')
 git = tf.constant('Hello, Git')
@@ -12,7 +16,7 @@ a = tf.constant(10)
 b = tf.constant(32)
 tf.print(a+b)
 
-# sess.close()
+# sess.close() """
 
 """ import tensorflow as tf
 tf.__version__ """
@@ -22,3 +26,7 @@ node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0)
 
 tf.print(node1,node2) """
+
+
+
+
